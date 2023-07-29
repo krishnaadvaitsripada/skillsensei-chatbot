bb22cfb5ae57b36893b376a61f2ef904242ea06e
Krishna Advait Sripada <114827450+krishnaadvaitsripada@users.noreply.github.com>
2023-07-30 04:22:43 +0530

    Wrote brief description of the chatbot

--- skillsensei_bot.py
+++ skillsensei_bot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title('Welcome to SkillSensei')
-st.write("Upload your resume here")
+st.title('Welcome to SkillSensei!')
+st.write("Interact with SkillSensei, the advanced chatbot capable of analyzing, comparing, and providing answers to any questions related to the uploaded resumes!")
 
 file = st.file_uploader("Upload Resume")
 
