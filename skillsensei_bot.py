616bcd7b1a44aa4d572e93c049288f8514f24881
Krishna Advait Sripada <sripada.advait@gmail.com>
2023-07-30 05:30:04 +0530

    Added a sidebar

--- skillsensei_bot.py
+++ skillsensei_bot.py
@@ -1,7 +1,13 @@
 import streamlit as st
 
-st.title('Welcome to SkillSensei!')
+st.title('Welcome to SkillSensei! 🤖')
 st.write("Interact with SkillSensei, the advanced chatbot capable of analyzing, comparing, and providing answers to any questions related to the uploaded resumes!")
 
 file = st.file_uploader("Upload Resume")
 
+with st.sidebar:
+    st.title('SkillSensei')
+    st.subheader("by Krishna Advait Sripada & Tarak Ram")
+    st.caption("© 2023 by SkillSensei")
+        
+input_msg = st.text_input("Ask a question!")
