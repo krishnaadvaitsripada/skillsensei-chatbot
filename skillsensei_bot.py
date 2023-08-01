8d94672807fab8e5b952571606d49bcd77a8bf0c
Krishna Advait Sripada <sripada.advait@gmail.com>
2023-08-02 03:51:21 +0530

    Implemented memory functionality

--- skillsensei_bot.py
+++ skillsensei_bot.py
@@ -5,6 +5,20 @@ from PyPDF2 import PdfReader
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) 
 
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
+from langchain.memory import ConversationTokenBufferMemory, ConversationSummaryBufferMemory
+
+llm = ChatOpenAI(temperature=0.0)
+memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=200)
+conversation = ConversationChain(
+    llm=llm, 
+    memory=memory,
+    verbose=False
+)
+
 def extract_text_from_pdf(file):
     pdf_document = PdfReader(file)
     text = ""
@@ -15,7 +29,7 @@ def extract_text_from_pdf(file):
 def generate_answer(question, resume_text):
     max_prompt_length = 4096 - len(f"Question: {question}\nResume: ")
     truncated_resume = resume_text[:max_prompt_length].rsplit("\n", 1)[0]
-
+    history_answer = conversation.predict(input=question)
     messages = [
         {
             'role': 'system',
@@ -25,7 +39,9 @@ def generate_answer(question, resume_text):
                 Here is the text provided in the uploaded PDF document: {truncated_resume} \
                 Analyze the resume text provided and answer any queries the user may have pertaining to the content \
                 in the document. If the user uses any inappropriate language, please warn them. \
-                All your responses must be appropriately long and no more than the token limit of {max_prompt_length}."
+                All your responses must be appropriately long and no more than the token limit of {max_prompt_length}. \
+                If any of the user's questions appear to reference a prior conversation, the appropriate \
+                response should be obtained using {history_answer}"
         },
         {
             'role': 'user',
@@ -56,12 +72,17 @@ def main():
 
     if file is not None:
         resume_text = extract_text_from_pdf(file)
-
+        
         # Ask questions
-        question = st.text_input("Enter your Question:")
-        if question:
-            answer = generate_answer(question, resume_text)
-            st.text_area("Answer:", value=answer, height=200)
+        question = st.text_input("Enter your Question:", key="first_question")
+        i = 0
+        while True:
+            if question:
+                answer = generate_answer(question, resume_text)
+                memory.save_context({"input": question}, {"output": answer})
+                st.text_area("Answer:", value=answer, height=200)
+                question = st.text_input("Enter your next question:", key="next_question"+str(i))
+                i+=1 # to avoid duplicate keys on the previous line
 
 if __name__ == "__main__":
     main()
