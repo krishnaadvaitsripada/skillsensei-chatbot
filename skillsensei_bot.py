bcfab1f922445a92074f8c55873fd48f9262c704
Tarak Ram <jujjurutarakram1818@gmail.com>
2023-08-01 23:01:30 +0530

    check this

--- skillsensei_bot.py
+++ skillsensei_bot.py
@@ -2,6 +2,8 @@ import streamlit as st
 import openai
 import os
 from PyPDF2 import PdfReader
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) 
 
 def extract_text_from_pdf(file):
     pdf_document = PdfReader(file)
@@ -32,7 +34,7 @@ def generate_answer(question, resume_text):
     ]
 
     # Set the OpenAI API key
-    openai.api_key = 'API Key'
+    openai.api_key = os.environ['OPENAI_API_KEY']
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -43,7 +45,11 @@ def generate_answer(question, resume_text):
 
 def main():
     st.title('Welcome to SkillSensei! 🤖')
-	@@ -67,45 +52,16 @@ def main():
+    st.write("Interact with SkillSensei, the advanced chatbot capable of analyzing, comparing, and providing answers to any questions related to the uploaded resumes!")
+
+    file = st.file_uploader("Upload Resume", type=["pdf"])
+
+    with st.sidebar:
         st.title('SkillSensei')
         st.subheader("by Krishna Advait Sripada & Tarak Ram")
         st.caption("© 2023 by SkillSensei")
@@ -56,3 +62,6 @@ def main():
         if question:
             answer = generate_answer(question, resume_text)
             st.text_area("Answer:", value=answer, height=200)
+
+if __name__ == "__main__":
+    main()
