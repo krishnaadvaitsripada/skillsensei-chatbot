7a64ac263e7fd6cc42c3ab54f88e55317d5e2704
Krishna Advait Sripada <sripada.advait@gmail.com>
2023-08-03 02:15:40 +0530

    Implemented Multiple PDFs functionality

--- skillsensei_bot.py
+++ skillsensei_bot.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import openai
 import os
+import pgml
 from PyPDF2 import PdfReader
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) 
@@ -29,19 +30,19 @@ def extract_text_from_pdf(file):
         text += page.extract_text()
     return text
 
-def generate_answer(question, resume_text):
-    max_prompt_length = 4096 - len(f"Question: {question}\nResume: ")
-    truncated_resume = resume_text[:max_prompt_length].rsplit("\n", 1)[0]
+def generate_answer(question, resume_texts):
+    max_prompt_length = 4096 - len(f"Question: {question}\nResumes: ")
+    truncated_resumes = " ".join(resume[:max_prompt_length] for resume in resume_texts)
     history_answer = conversation.predict(input=question)
     messages = [
         {
             'role': 'system',
             'content': f"You are an assistant by the name of SkillSensei who \
                 responds in a formal, professional, and polite manner. The user has \
-                provided you with a list of PDF documents (either 1 or more) that are resumes of candidates. \
-                Here is the text provided in the uploaded PDF document: {truncated_resume} \
-                Analyze the resume text provided and answer any queries the user may have pertaining to the content \
-                in the document. If the user uses any inappropriate language, please warn them. \
+                provided you with {len(resume_texts)} PDF documents that are resumes of candidates. \
+                Here are the first few words of each uploaded resume: {truncated_resumes} \
+                Analyze the resume texts provided and answer any queries the user may have pertaining to the content \
+                in the documents. If the user uses any inappropriate language, please warn them. \
                 All your responses must be appropriately long and no more than the token limit of {max_prompt_length}. \
                 If any of the user's questions appear to reference a prior conversation, the appropriate \
                 response should be obtained using {history_answer}"
@@ -63,27 +64,29 @@ def main():
     st.title('Welcome to SkillSensei! 🤖')
     st.write("Interact with SkillSensei, the advanced chatbot capable of analyzing, comparing, and providing answers to any questions related to the uploaded resumes!")
 
-    file = st.file_uploader("Upload Resume", type=["pdf"])
+    files = st.file_uploader("Upload Resumes (up to 20)", type=["pdf"], accept_multiple_files=True)
+    resume_texts = []
 
     with st.sidebar:
         st.title('SkillSensei')
         st.subheader("by Krishna Advait Sripada & Tarak Ram")
         st.caption("© 2023 by SkillSensei")
 
-    if file is not None:
-        resume_text = extract_text_from_pdf(file)
-        
+    if files is not None:
+        for file in files:
+            resume_text = extract_text_from_pdf(file)
+            resume_texts.append(resume_text)
+
         # Ask questions
         question = st.text_input("Enter your Question:", key="first_question")
         i = 0
         while True:
             if question:
-                answer = generate_answer(question, resume_text)
+                answer = generate_answer(question, resume_texts)
                 memory.save_context({"input": question}, {"output": answer})
                 st.text_area("Answer:", value=answer, height=200)
                 question = st.text_input("Enter your next question:", key="next_question"+str(i))
-                i+=1 # to avoid duplicate keys on the previous line
+                i += 1 # to avoid duplicate keys on the previous line
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     main()
-
